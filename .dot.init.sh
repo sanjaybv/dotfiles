0c5531d4bd18445d491eb88e6c93bcb4e61a816b
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2022-05-03 20:57:52 -0700

    Add init script

---
+++ .dot.init.sh
@@ -0,0 +1,24 @@
+# created from https://www.atlassian.com/git/tutorials/dotfiles
+
+# initialize the repo
+git init --bare $HOME/.dot
+
+# clone the repo
+git clone --bare git@github:sanjaybv/dotfiles.git $HOME/.dot
+
+# set the alias for the current shell
+alias dot='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+# checkout and backup if necessary.
+dot checkout
+if [ $? = 0 ]; then
+	echo "checked out dot";
+else
+	echo "backing up existing dotfiles to .dot.backup";
+	mkdir -p .dot.backup
+	dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dot.backup/{}
+fi;
+dot checkout
+
+# no need to see untracked files
+dot config status.showUntrackedFiles no
