88d2cd941e243dd928e9cc0ee21c2655a23c4b88
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2022-05-12 16:36:39 -0700

    install gh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 # Install packages
 if [ "$(uname)" == "Darwin" ]; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	brew install chezmoi tmux vim git cmake python go nodejs starship fzf
+	brew install chezmoi tmux vim git cmake python go nodejs starship fzf gh
 else if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	# For gh cli tool
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+	sudo apt update
 	snap install chezmoi starship
-	apt install tmux vim git cmake python go nodejs fzf
+	apt install tmux vim git cmake python go nodejs fzf gh
 else
 	echo "unknown OS environment $(uname -s). Install the following packages manually."
-	echo "chezmoi tmux vim git cmake python go nodejs starship fzf"
+	echo "chezmoi tmux vim git cmake python go nodejs starship fzf gh"
 	read -n 1 -r -p "Press any key to continue"
 fi;
 
