d8453386c1241b514524d8f1ce5e97903803f4b7
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2023-12-20 22:33:21 -0800

    fix tmux and use tab to complete

--- dot_config/nvim/lua/plugins/custom.lua
+++ dot_config/nvim/lua/plugins/custom.lua
@@ -52,7 +52,7 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
+            cmp.confirm({ select = true })
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
             -- this way you will only jump inside the snippet region
           elseif luasnip.expand_or_jumpable() then
